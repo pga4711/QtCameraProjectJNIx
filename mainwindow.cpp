6ba13919936cabec2234a37a348e1747ded2961c
Philip G <pga4711@gmail.com>
2018-03-28 08:54:03 +0200

    Deleted some comments.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -38,29 +38,5 @@ void MainWindow::startCamera()
         qDebug()<<"qtnativeActivity is valid";
     qDebug()<<"qtnativeActivity is NOT valid";
 
-    /*
-    QAndroidJniObject intent("android/content/Intent", <--------Där är klassen intent, man skapar en android-intent
-                             "(Ljava/lang/String;)V",  <----------Argumentet är en String som vi skickar in, och ut kommer void!
-                             ACTION_SET_TIMER.object<jstring>()); <--------jstring skickas in, då vet jni om det. .object<jstring>()
-                                                                           gör om android-stringt till en JNI-vänlig string
 
-     */
-
-    //Prova gör en sånhär:
-    //    Intent intent = new Intent(this, SecondActivity.class);
-    //    startActivity(intent);
-    //Som implementeras såhär:
-    // public Intent(Context packageContext, Class<?> cls) {
-    //  throw new RuntimeException("Stub!");
-    //}
-
-
-
-    //fast i Qt-JNI-form. HUR SKA JAG GÖRA DET?
-    //QAndroidJniObject intent("android/content/Intent",
-    //                         "(Landroid/content/Context;Ljava/lang.Class;)V");
-
-    //QAndroidJniObject mainActivity = QtAndroid::androidActivity();
-
-    //QtAndroid::startActivity(activity);
 }
