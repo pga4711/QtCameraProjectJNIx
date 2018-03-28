6fb894aa53212f354f6fe075f54e620f3a6ac752
Philip G <pga4711@gmail.com>
2018-03-28 17:41:44 +0200

    Now, there are no compilation error. We added a compile  statement in the gradle build file. Thanks to Qt Forums. Now we trying to start my StartCameraActivity.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,7 +12,7 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     startCamera();
-    qDebug()<<"TEST";
+
 }
 
 MainWindow::~MainWindow()
@@ -34,9 +34,87 @@ void MainWindow::startCamera()
             QAndroidJniObject::callStaticObjectMethod("org/qtproject/qt5/android/QtNative",
                                                       "activity",
                                                       "()Landroid/app/Activity;");
+
+
     if (qtnativeActivity.isValid())
+    {
         qDebug()<<"qtnativeActivity is valid";
+        //QAndroidJniObject intent("android/content/Intent", "()V");
+
+        QAndroidJniObject param1 = QAndroidJniObject::fromString("com.vmi");
+        QAndroidJniObject param2 = QAndroidJniObject::fromString("com.vmi.StartCameraActivity");
+
+
+
+        QAndroidJniObject intent("android/content/Intent", "()V");
+
+        /*
+        QAndroidJniObject intent("android/content/Intent",
+                                 "(Landroid/content/Context;Ljava/lang/Class)V"
+                                 qtnativeActivity.object<jobject>(),"min activity!!!!");
+        */
+
+        // Landroid/content/Context;   och sen ska vi ha Ljava/lang/Class;
+        if (intent.isValid()){
+            qDebug()<<"INTENT IS VALID";
+            intent.callObjectMethod("setClassName","(Ljava/lang/String;Ljava/lang/String;)Landroid/content/Intent;",
+                                    param1.object<jobject>(), param2.object<jobject>());
+
+
+
+            //Här är problemet just nu.
+            //qtnativeActivity.callObjectMethod("startActivity","(Landroid/content/Intent;)V",intent.object<jobject>());
+        }
+
+    }
+    else
+    {
     qDebug()<<"qtnativeActivity is NOT valid";
+    }
+    if (mainActivity.isValid())
+    {
+        qDebug()<<"mainactivity is valid";
+    }
+    else
+    {
+    qDebug()<<"mainactivity is NOT valid";
+    }
+    /*
+        QAndroidJniObject intent("android/content/Intent", <--------Där är klassen intent, man skapar en android-intent
+                                 "(Ljava/lang/String;)V",  <----------Argumentet är en String som vi skickar in, och ut kommer void!
+                                 ACTION_SET_TIMER.object<jstring>()); <--------jstring skickas in, då vet jni om det. .object<jstring>()
+                                                                               gör om android-stringt till en JNI-vänlig string
+
+         */
+
+        //Prova gör en sånhär:
+        //    Intent intent = new Intent(this, SecondActivity.class);
+        //    startActivity(intent);
+        //Som implementeras såhär:
+        // public Intent(Context packageContext, Class<?> cls) {
+        //  throw new RuntimeException("Stub!");
+        //}
+
+
+        //fast i Qt-JNI-form. HUR SKA JAG GÖRA DET?
+        //QAndroidJniObject intent("android/content/Intent",
+        //                         "(Landroid/content/Context;Ljava/lang.Class;)V");
+
+        //QAndroidJniObject mainActivity = QtAndroid::androidActivity();
+
+        //QtAndroid::startActivity(activity);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
